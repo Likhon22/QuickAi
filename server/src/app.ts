7bbbc3dec385cecc4bd87fc2bfe5f6417b1defaf
Likhon22 <likhonsarker793@gmail.com>
2025-08-12 10:21:15 +0600

    create the server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,15 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const test = (req: Request, res: Response) => {
+  res.send("server is running");
+};
+app.get("/", test);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
